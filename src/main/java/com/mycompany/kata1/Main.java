e1592d1ddacff2b33d017c6b0388357b907cfed5
Carlos Santana <Carlos@Carlos>
2020-10-07 09:57:41 +0100

    Primera versión

---
+++ src/main/java/com/mycompany/kata1/Main.java
@@ -0,0 +1,19 @@
+package com.mycompany.kata1;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+/**
+ *
+ * @author Carlos Santana Rodriguez
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Person person = new Person ("Carlos Santana", LocalDate.of(2000, Month.MARCH, 21));
+        System.out.println("La persona se llama " + person.getName()+ " y tiene " + person.getAge() + " años");
+        
+    }
+    
+}
---
+++ src/main/java/com/mycompany/kata1/Person.java
@@ -0,0 +1,43 @@
+package com.mycompany.kata1;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Carlos
+ */
+
+public class Person {
+
+    private final String name;
+    private final LocalDate birthdate;
+    
+    public Person(String name, LocalDate birthdate) {
+        
+        this.name = name;
+        this.birthdate = birthdate;
+
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+    
+    public int getAge(){
+        return toYears(LocalDate.now().toEpochDay() - birthdate.toEpochDay());
+    }
+
+    private int toYears(long days) {
+        return (int) (days/365.25);
+    }
+    
+    @Override
+    public String toString() {
+        return "Person{"+"name="+name+", birthdate="+birthdate+"}";
+    }
+    
+}
